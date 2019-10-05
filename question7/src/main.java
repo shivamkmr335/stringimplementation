4d08b5b824b2aeace874f4be0253aec3591f2c9c
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 21:44:48 +0530

    taking input from the user and creating a byte array having ascii  values of every characters  of given string

--- question7/src/main.java
+++ question7/src/main.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class main {
     public static void main(String[]  args) {
         Scanner s = new Scanner(System.in);
+        String str=s.nextLine();
+        byte b[]=str.getBytes();
+
     }
 }
