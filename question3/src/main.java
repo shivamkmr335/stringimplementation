16eb8cf7a970102967801ce9658a678d290e44db
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 20:22:28 +0530

    performing some change to get requires output

--- question3/src/main.java
+++ question3/src/main.java
@@ -4,6 +4,7 @@ public class main {
     public static void main(String[] args){
         Scanner s=new Scanner(System.in);
         String st=s.nextLine();
-        System.out.println();
+        replacedstring rep=new replacedstring();
+        rep.replace(st);
     }
 }
