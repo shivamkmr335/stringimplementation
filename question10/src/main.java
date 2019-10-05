ccf88afa01ea7dacf3d459aeff423488d68276c7
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 22:22:50 +0530

    taking all required input from the user

--- question10/src/main.java
+++ question10/src/main.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class main {
     public static void main(String[]  args) {
         Scanner s = new Scanner(System.in);
+        System.out.println("enter the string :");
         String str = s.nextLine();
+        System.out.println("enter charater");
+        char ch=s.nextLine().charAt(0);
 
     }
 }
