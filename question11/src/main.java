c43b5ba3fd35a0c3b5afb23a8f1b7a64254e3a0a
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 22:33:21 +0530

    importing scanner class and creating it's object declaring main method taking all the required input from the user

--- question11/src/main.java
+++ question11/src/main.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[]  args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter the string :");
+        String str = s.nextLine();
+        System.out.println("enter charater");
+        char ch = s.nextLine().charAt(0);
+    }
 }
