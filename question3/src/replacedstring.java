d64ef3b5da68d336a869c82496bae2d1345545c9
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 20:00:23 +0530

    creating replace to be able to replace vowels with * in a given string

--- question3/src/replacedstring.java
+++ question3/src/replacedstring.java
@@ -2,7 +2,17 @@ public class replacedstring {
     public  String s;
     public char c='*';
     public String replace(String str) {
-
+        int len=str.length();
+        for (int i = 0; i <len ; i++) {
+            char st=str.charAt(i);
+            if(st=='a' || st=='e' || st=='o' || st=='i' || st=='u') {
+                s.concat("*");
+            }
+            else{
+                s.concat(String.valueOf(str.charAt(i)));
+            }
+        }
         return s;
     }
+
 }
