dbd385b72463416e0719a71772599f18ebf2351c
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 19:21:44 +0530

    giving formatted output to  the user

--- question1/src/main.java
+++ question1/src/main.java
@@ -5,8 +5,14 @@ public class main {
         Scanner s=new Scanner(System.in);
         String str=s.nextLine();
         String[] st=new String[str.length()];
+        System.out.print("index:\t");
         for(int i = 0; i <str.length() ; i++){
             st[i] = String.valueOf(str.charAt(i));
+            System.out.print(i+"\t");
+        }
+        System.out.print("\nchars:\t");
+        for (int i = 0; i <str.length() ; i++) {
+            System.out.print(st[i]+"\t");
         }
 
     }
