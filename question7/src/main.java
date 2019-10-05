e932bf0f7a16c6cf8040b8c731e54fda7e8ef185
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 21:47:15 +0530

    using b[] array to pring sum of ascii values of given string

--- question7/src/main.java
+++ question7/src/main.java
@@ -5,6 +5,10 @@ public class main {
         Scanner s = new Scanner(System.in);
         String str=s.nextLine();
         byte b[]=str.getBytes();
-
+        int sum=0;
+        for (int i = 0; i <str.length() ; i++) {
+            sum=sum+b[i];
+        }
+        System.out.print("TotalWeight = "+sum);
     }
 }
