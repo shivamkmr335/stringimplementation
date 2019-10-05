4ee8f8e4bbbf6fa7f6852b7755f0891be87786ff
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 19:16:41 +0530

    creating a string array st and setting values as characters of input String

--- question1/src/main.java
+++ question1/src/main.java
@@ -4,5 +4,10 @@ public class main {
     public static void main(String[] args){
         Scanner s=new Scanner(System.in);
         String str=s.nextLine();
+        String[] st=new String[str.length()];
+        for(int i = 0; i <str.length() ; i++){
+            st[i] = String.valueOf(str.charAt(i));
+        }
+
     }
 }
