f0d5b09afa6320b45fb545027521d76a882578e4
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 20:42:17 +0530

    declaring public char array creating public modify  method and proramming it to  provide required  output

--- question4/src/modifystr.java
+++ question4/src/modifystr.java
@@ -1,2 +1,16 @@
 public class modifystr {
+    public char[] ch={'*','^','!'};
+    public void modify(String str){
+        int count=0;
+        for (int i = 0; i <str.length() ; i++) {
+            char st=str.charAt(i);
+            if (st == 'a' || st == 'e' || st == 'o' || st == 'i' || st == 'u') {
+                System.out.print(ch[count%3]);
+                count++;
+            }
+            else{
+                System.out.print(st);
+            }
+        }
+    }
 }
