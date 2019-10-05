eabf48d89a139aa61693435c66a27974069c4655
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 21:26:40 +0530

    using toUpperCase method of String class to create a upper case string of the given string

--- question6/src/main.java
+++ question6/src/main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class main {
     public static void main(String[]  args){
         Scanner s=new Scanner(System.in);
-
+        String str=s.nextLine();
+        String str2=str.toUpperCase();
+        System.out.println(str2);
     }
 }
