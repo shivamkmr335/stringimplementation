2941f875123b68f65b9dd5191aaf718317bcd3ef
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 21:18:42 +0530

    creating object of scanner class and taking all input from the user

--- question5/src/main.java
+++ question5/src/main.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[]  args){
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter base string:");
+        String basestr=s.nextLine();
+        System.out.println("enter substring to be raplaced");
+        String substr=s.nextLine();
+        System.out.println("enter string  to be added to given string");
+        String repstr=s.nextLine();
+
+    }
 }
