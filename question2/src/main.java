e66e0be2a77e3cdce2fe5452b5f0085171fb7213
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 19:28:14 +0530

    taking input from user for string and index

--- question2/src/main.java
+++ question2/src/main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args){
         Scanner s=new Scanner(System.in);
+        System.out.println("enter string:");
         String str=s.nextLine();
+        System.out.println("enter index for substring");
+        int index=Integer.parseInt(s.nextLine());
     }
 }
