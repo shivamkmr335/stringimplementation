2f2b6bc939b64accdb2bdf941d8647e12c38d1f0
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 21:57:45 +0530

    creating upper case char array from the given string and adding all the ascii of upper case values of given string

--- question8/src/main.java
+++ question8/src/main.java
@@ -5,6 +5,11 @@ public class main {
         Scanner s = new Scanner(System.in);
         String str=s.nextLine();
         String st=str.toUpperCase();
-
+        byte b[]=st.getBytes();
+        int sum=0;
+        for (int i = 0; i <st.length() ; i++) {
+            sum=sum+b[i];
+        }
+        System.out.print("TotalWeight = "+sum);
     }
 }
