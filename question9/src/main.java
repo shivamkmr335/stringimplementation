b3ebe23c4bc01f941ef65cd12c8a361cfac478b0
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 22:01:24 +0530

    creating object and importing scanner class declaring main method taking input from the user

--- question9/src/main.java
+++ question9/src/main.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[]  args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+
+    }
 }
