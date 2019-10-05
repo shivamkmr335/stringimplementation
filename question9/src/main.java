fff5893c91d881c41e70e4709bf37283c9a3af46
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-10-05 22:07:32 +0530

    creating upper  and lower case strings of the given string and creating byte array storing the ascii values of new string created

--- question9/src/main.java
+++ question9/src/main.java
@@ -4,6 +4,12 @@ public class main {
     public static void main(String[]  args) {
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
+        String upper=str.toUpperCase();
+        String lower=str.toLowerCase();
+        byte chUpper[]=upper.getBytes();
+        byte chLower[]=lower.getBytes();
+        int low=0;
+        int up=0;
 
     }
 }
